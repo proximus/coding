9055264e596068c65823dc147d4f2c5607127930
Samuel Gabrielsson <samuel.gabrielsson@gmail.com>
2015-08-30 21:21:28 +0200

    Add doxygen

--- C++/C++.for.Mathematicians/Program.1.1-Hello.World/src/main.cpp
+++ C++/C++.for.Mathematicians/Program.1.1-Hello.World/src/main.cpp
@@ -1,7 +1,31 @@
+/*******************************************************************************
+ *
+ *         FILE: main.cpp
+ *
+ *        USAGE: ---
+ *
+ *  DESCRIPTION: ---
+ *
+ *      OPTIONS: ---
+ * REQUIREMENTS: ---
+ *         BUGS: ---
+ *        NOTES: ---
+ *       AUTHOR: Samuel Gabrielsson (samuel.gabrielsson@gmail.com)
+ * ORGANIZATION: ---
+ *      VERSION: 1.0
+ *      CREATED: 2015-08-30 19:41:53
+ *     REVISION: ---
+ *      CHANGES: ---
+ *
+ ******************************************************************************/
 #include <iostream>
-
 using namespace std;
 
+/*! \brief Brief description.
+ *         Brief description continued.
+ *
+ *  Detailed description starts here.
+ */
 int main()
 {
     cout << "Hello, World!" << endl;
